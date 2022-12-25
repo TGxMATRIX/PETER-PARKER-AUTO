2204408e6694b1b41a0a9c2c9f90b2d506482b1f
MatRixBotz <108185896+matrixbotz0@users.noreply.github.com>
2022-12-25 10:36:28 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ then
   gh repo clone matrixbotz0/PETER-PARKER-BOT
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /FiltersBot
+  git clone $UPSTREAM_REPO /PETER-PARKER-BOT
 fi
-cd /FiltersBot
+cd /PETER-PARKER-BOT
 pip3 install -U -r requirements.txt
 echo "Starting Bot..."
 python3 bot.py
