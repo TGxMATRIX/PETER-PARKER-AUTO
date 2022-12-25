981e5030fd678ba54ba5350eaf5ff0f200cd1b0d
MatRixBotz <108185896+matrixbotz0@users.noreply.github.com>
2022-12-25 10:35:53 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/HansakaAnuhas/FiltersBot.git /FiltersBot
+  gh repo clone matrixbotz0/PETER-PARKER-BOT
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /FiltersBot
