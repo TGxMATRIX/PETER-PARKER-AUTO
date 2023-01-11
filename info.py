95bdf5a281cfc8435de33015c56337f5749cf6cb
MatRixBotz <108185896+matrixbotz0@users.noreply.github.com>
2023-01-11 15:11:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -24,7 +24,7 @@ API_HASH = environ['API_HASH']
 BOT_TOKEN = environ['BOT_TOKEN']
 
 # Bot settings
-CACHE_TIME = int(environ.get('CACHE_TIME', 300))
+CACHE_TIME = int(environ.get('CACHE_TIME', 999))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 PICS = (environ.get('PICS' ,'https://telegra.ph/file/79bd83439254ed9c4bbca.jpg')).split()
 BOT_START_TIME = time()
