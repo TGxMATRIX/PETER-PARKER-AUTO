97f94dba55fd72ba5c4d31f92c6f25ca904ef7f6
CrazyDeveloperTG <119148554+CrazyDeveloperTG@users.noreply.github.com>
2023-01-14 09:54:22 +0530

    Set buttons

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -823,7 +823,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
             InlineKeyboardButton('🤖 Updates', url='https://t.me/MatRixBotzTG')
         ], [
-            InlineKeyboardButton('ℹ️ Help', callback_data='help'),
+            InlineKeyboardButton('ℹ️ Help', callback_data='help2'),
             InlineKeyboardButton('😊 About', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
