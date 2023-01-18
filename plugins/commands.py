1dac5af90a2620d4d3ebde197b55c7ae590a2316
Hansaka Anuhas <121796740+HansakaAnuhas@users.noreply.github.com>
2023-01-18 14:37:31 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -468,6 +468,16 @@ async def settings(client, message):
                     callback_data=f'setgs#welcome#{settings["welcome"]}#{grp_id}',
                 ),
             ],
+            [
+                InlineKeyboardButton(
+                    'ShortLink',
+                    callback_data=f'setgs#is_shortlink#{settings["is_shortlink"]}#{grp_id}',
+                ),
+                InlineKeyboardButton(
+                    '✅ 𝐘𝐄𝐒' if settings["is_shortlink"] else '🗑️ 𝐍𝐎',
+                    callback_data=f'setgs#is_shortlink#{settings["is_shortlink"]}#{grp_id}',
+                ),
+            ],
         ]
 
         reply_markup = InlineKeyboardMarkup(buttons)
