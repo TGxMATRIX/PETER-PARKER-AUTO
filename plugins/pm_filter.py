f013d03fc9a09e480457040653a09ae2fcffa058
Hansaka Anuhas <121796740+Hansaka-Anuhas@users.noreply.github.com>
2023-02-23 12:10:29 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -490,23 +490,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
         if f_caption is None:
             f_caption = f"{files.file_name}"
 
-        if query.message.chat.type == enums.ChatType.PRIVATE:
-            await query.answer()
-            await client.send_cached_media(
-                chat_id=query.from_user.id,
-                file_id=file_id,
-                caption=f_caption,
-                protect_content=True if ident == "filep" else False,
-                reply_markup=InlineKeyboardMarkup(
-                    [
-                        [
-                            InlineKeyboardButton("🔥 ᴄʜᴀɴɴᴇʟ 🔥", url=(MAIN_CHANNEL))
-                        ]
-                    ]
-                )
-            )
-            return
-
         try:
             if AUTH_CHANNEL and not await is_subscribed(client, query):
                 if clicked == typed:
