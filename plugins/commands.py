3d19e14b594d88eff6394254d2e10e60bb9131b2
Matrix TG <108185896+matrixbotz0@users.noreply.github.com>
2023-02-23 15:10:37 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -422,7 +422,7 @@ async def settings(client, message):
                     callback_data=f'setgs#botpm#{settings["botpm"]}#{grp_id}',
                 ),
                 InlineKeyboardButton(
-                    'Bot PM' if settings["botpm"] else 'Channel',
+                    'Bot PM' if settings["botpm"] else 'AUTO SEND',
                     callback_data=f'setgs#botpm#{settings["botpm"]}#{grp_id}',
                 ),
             ],
