24a06798e00bbef78d166df5119686913d7a72f3
Matrix TG <108185896+matrixbotz0@users.noreply.github.com>
2023-03-06 17:38:50 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -11,6 +11,7 @@ from database.users_chats_db import db
 from info import CHANNELS, ADMINS, AUTH_CHANNEL, LOG_CHANNEL, PICS, BATCH_FILE_CAPTION, CUSTOM_FILE_CAPTION, PROTECT_CONTENT, MSG_ALRT, MAIN_CHANNEL, REQ_CHANNEL
 from utils import get_settings, get_size, is_subscribed, save_group_settings, temp
 from database.connections_mdb import active_connection
+from plugins.fsub import ForceSub
 import re
 import json
 import base64
