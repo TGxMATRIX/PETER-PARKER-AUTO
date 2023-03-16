5381e76369158c8fd4bf0b7355d303456186bb41
Muhammedshifin <muhammedshifink4@gmail.com>
2023-03-16 17:47:39 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1298,7 +1298,8 @@ async def auto_filter(client, msg, spoll=False):
             btn = [
                 [
                     InlineKeyboardButton(
-                        text=f"[{get_size(file.file_size)}] {file.file_name}", callback_data=f'{pre}#{file.file_id}'
+                        text=f"[{get_size(file.f
+                        text=f"[{get_size(file.file_size)}] {file.file_name}", callback_data=f'{pre}#{file.file_idile_size)}] {file.file_name}", callback_data=f'{pre}#{req}#{file.file_id}'
                     ),
                 ]
                 for file in files
@@ -1308,11 +1309,11 @@ async def auto_filter(client, msg, spoll=False):
                 [
                     InlineKeyboardButton(
                         text=f"{file.file_name}",
-                        callback_data=f'{pre}#{file.file_id}',
+                        callback_data=f'{pre}#{req}#{file.file_id}',
                     ),
                     InlineKeyboardButton(
                         text=f"{get_size(file.file_size)}",
-                        callback_data=f'{pre}#{file.file_id}',
+                        callback_data=f'{pre}#{req}#{file.file_id}',
                     ),
                 ]
                 for file in files
