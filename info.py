ffddd3345d2840045bcd56e1f99a4509c8cb6040
ᴀꜰsᴀʟ ʜᴀsʜɪᴍ​ <123615887+CyberTG@users.noreply.github.com>
2023-03-26 13:06:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -103,8 +103,8 @@ JOIN_REQS_DB = environ.get("JOIN_REQS_DB", DATABASE_URI)
 
 # Others
 BUTTON_LOCK_TEXT = environ.get("BUTTON_LOCK_TEXT", "⚠️ 𝙃𝙚𝙮 {query}! 𝙏𝙝𝙖𝙩'𝙨 𝙉𝙤𝙩 𝙁𝙤𝙧 𝙔𝙤𝙪. 𝙋𝙡𝙚𝙖𝙨𝙚 𝙍𝙚𝙦𝙪𝙚𝙨𝙩 𝙔𝙤𝙪𝙧 𝙊𝙬𝙣")
-BUTTON_LOCK = environ.get("BUTTON_LOCK", "False")
-NO_RESULTS_MSG = is_enabled((environ.get('NO_RESULTS_MSG', "False")), True)
+BUTTON_LOCK = environ.get("BUTTON_LOCK", "True")
+NO_RESULTS_MSG = bool(environ.get('NO_RESULTS_MSG', False))
 SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 PORT = os.environ.get("PORT", "8080")
