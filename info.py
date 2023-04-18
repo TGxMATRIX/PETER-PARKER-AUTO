8809984cb871e2884c52f7311fce752ccc67735c
Matrix TG <108185896+matrixbotz0@users.noreply.github.com>
2023-04-18 22:51:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -100,7 +100,7 @@ APPROVED = environ.get("APPROVED_WELCOME", "on").lower()
 auth_channel = environ.get('AUTH_CHANNEL')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 # Set to False inside the bracket if you don't want to use Request Channel else set it to Channel ID
-REQ_CHANNEL = environ.get("REQ_CHANNEL", -1001766697373), False)
+REQ_CHANNEL = environ.get("REQ_CHANNEL", "-1001766697373")
 REQ_CHANNEL = int(REQ_CHANNEL) if REQ_CHANNEL and id_pattern.search(REQ_CHANNEL) else False
 JOIN_REQS_DB = environ.get("JOIN_REQS_DB", DATABASE_URI)
 
